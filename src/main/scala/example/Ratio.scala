682d03acd14045ce9b0ed369739c946fdc3ea39c
Yaroslav <yaroslav.sydorenko@cosmonova.net>
2018-07-06 16:52:31 +0300

    Implements compare list of Ratio

--- src/main/scala/example/Ratio.scala
+++ src/main/scala/example/Ratio.scala
@@ -3,3 +3,11 @@ package example
 final case class Ratio(a: Int, b: Int) {
   def ===(that: Ratio): Boolean = a * that.b == b * that.a
 }
+
+object Ratio {
+  def compare(first: List[Ratio], second: List[Ratio]): Boolean = {
+    first.size == second.size && first.zip(second).forall {
+      case (r1, r2) => r1 === r2
+    }
+  }
+}
